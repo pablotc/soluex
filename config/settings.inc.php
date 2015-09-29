9eabacb26285a6b35dadb1b1a805eaad51f7d9b4
Ubuntu <ubuntu@ip-172-31-21-130.us-west-2.compute.internal>
2015-09-29 13:49:26 +0000

    añadi archivo de idioma de version antigua para instalacion en español

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'ecommerce');
+define('_DB_USER_', 'soluex');
+define('_DB_PASSWD_', 'Soluex123');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 't7gnGpQdMRsTpr2BAcofI6JeOa8o70ydhpUJ3SXcgYHPFWqPjzaerdnr');
+define('_COOKIE_IV_', 'FQIxGqfZ');
+define('_PS_CREATION_DATE_', '2015-09-29');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.1');
+define('_RIJNDAEL_KEY_', '6whTk9mXzitQri2KQw1yTB9mBEDgOMkw');
+define('_RIJNDAEL_IV_', '4nKEth1SIf2vMl2YQ/i4PA==');
